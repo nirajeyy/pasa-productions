7bba088f9809f6ae8da03a1ffd732943f585cb70
nirajeyy <nirajtmg001@gmail.com>
2026-01-19 18:08:12 +0545

    Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
 	content: [
 		"./app/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
 		"./mdx-components.tsx",
 		"content/**/*.mdx",
 	],
